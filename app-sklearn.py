05195d25e3b6cfded4ca521e3feef0756d213693
rianaditro <rianaditro@gmail.com>
2024-04-23 20:24:30 +0700

    create pipeline

--- app-sklearn.py
+++ app-sklearn.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn import tree
+from sklearn.neighbors import KNeighborsClassifier  
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 
 
 
@@ -34,6 +38,40 @@ def transform_to_numeric(df:pd.DataFrame):
             df[col] = le.fit_transform(df[col])
     return df
 
+def main_pipeline(df:pd.DataFrame):
+    # split data
+    X = df.drop('spesies', axis=1)
+    y = df['spesies']
+    # cross validation using 10 fold
+    skf = StratifiedKFold(n_splits=10)
+
+    tree_algorithm = tree.DecisionTreeClassifier(criterion='entropy')
+    knn_algorithm = KNeighborsClassifier(n_neighbors=11)
+
+    folds_result = []
+    best_tree_model = None
+    best_tree_accuracy = 0.0
+
+    for train_index, test_index in skf.split(X,y):
+        # split data
+        X_train, X_test = X.loc[train_index], X.loc[test_index]
+        y_train, y_test = y.loc[train_index], y.loc[test_index]
+        # modeling
+        tree_model, tree_acc = model_pipeline(tree_algorithm, X_train, X_test, y_train, y_test)
+        
+        folds_result.append(tree_acc)
+        if tree_acc > best_tree_accuracy:
+            best_tree_accuracy = tree_acc
+            best_tree_model = tree_model
+    return best_tree_model, best_tree_accuracy, folds_result
+
+
+def model_pipeline(algorithm, X_train, X_test, y_train, y_test):
+    model = algorithm.fit(X_train, y_train)
+    prediction = model.predict(X_test)
+    accuracy = accuracy_score(prediction, y_test)
+    return model, accuracy
+
 
 
 if __name__ == "__main__":
@@ -44,4 +82,6 @@ if __name__ == "__main__":
     if uploaded_file is not None:
         df = preprocessing_data(uploaded_file)
         st.dataframe(df)
+
+        st.button('Create Model', on_click=main_pipeline(df))
         
