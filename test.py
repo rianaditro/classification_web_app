1ad5669e44ca7d5ae4c29cf3957b6daa371b3892
rianaditro <rianaditro@gmail.com>
2024-05-01 20:52:26 +0700

    to_sql ok

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
 import streamlit as st
 import pandas as pd
+from sqlalchemy.sql import text
+from sqlalchemy import create_engine
 
-conn = st.experimental_connection('databulu', type='sql')
 
+conn = st.connection('mysql', type='sql')
+cursor = conn._connect()
 
-if __name__ == "__main__":
-    df = conn.query("SELECT * FROM dummy")
-    st.write(df)
+with conn.session as s:
+    st.markdown(f"Note that `s` is a `{type(s)}`")
+    summ = pd.read_excel('db/clean_data_bulu.xlsx')
+    summ.to_sql('dataset', cursor, if_exists='replace')
+
+    # ins = text("CREATE TABLE IF NOT EXISTS pet_owners (person TEXT, pet TEXT)")
+    # s.execute(ins)
+    # # s.execute(text('DELETE FROM pet_owners;'))
+    # pet_owners = ['jerry', 'fish', 'barbara', 'cat', 'alex', 'puppy']
+    # s.execute(text(f'INSERT INTO pet_owners (person, pet) VALUES ("{pet_owners[4]}", "{pet_owners[5]}")'))
+    # s.commit()
+
+pet_owners = conn.query('select * from dataset')
+st.dataframe(pet_owners, hide_index=True)
